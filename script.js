5a035d0d574d9313bb7e0dab9749e73015e85e6a
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-05-20 14:09:25 +0500

    frist commit

---
+++ script.js
@@ -0,0 +1,22 @@
+// // Print odd numbers and even numbers
+
+// function checkValue() {
+//     var a = document.getElementById('input').value;
+
+//     if (a == 1 || a == 3 || a == 5 || a == 7 || a == 9) {
+//         alert("odd Number");
+//     } else if(a == 0 || a == 2 || a == 4 || a == 6 || a == 8) {
+//         alert('even Number');
+//     } else{
+//         alert('Bro this is not a even number or odd number is this randon number')
+//     }
+// } 
+
+//!  scop global, block, functional 
+
+var a = 
+{
+    var a = 10;
+}
+
+console.log(a)
