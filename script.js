bc82b0f35c418ae2466d35a81ca2f2c490cd0cbc
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-05-20 15:07:27 +0500

    scop global, block, functional

--- script.js
+++ script.js
@@ -1,22 +1,31 @@
-// // Print odd numbers and even numbers
+// // // Print odd numbers and even numbers
 
-// function checkValue() {
-//     var a = document.getElementById('input').value;
+// // function checkValue() {
+// //     var a = document.getElementById('input').value;
 
-//     if (a == 1 || a == 3 || a == 5 || a == 7 || a == 9) {
-//         alert("odd Number");
-//     } else if(a == 0 || a == 2 || a == 4 || a == 6 || a == 8) {
-//         alert('even Number');
-//     } else{
-//         alert('Bro this is not a even number or odd number is this randon number')
-//     }
-// } 
+// //     if (a == 1 || a == 3 || a == 5 || a == 7 || a == 9) {
+// //         alert("odd Number");
+// //     } else if(a == 0 || a == 2 || a == 4 || a == 6 || a == 8) {
+// //         alert('even Number');
+// //     } else{
+// //         alert('Bro this is not a even number or odd number is this randon number')
+// //     }
+// // } 
 
-//!  scop global, block, functional 
+// //!  scop global, block, functional 
 
-var a = 
-{
-    var a = 10;
-}
+// * Global //
+// var a = 'rafay';
 
+// //* block //
+// {
+//     let b = 10;
+// }
+
+// //* functional //
+// function vars() {
+//     var c = 10;
+// }
+
+let a = 10
 console.log(a)
