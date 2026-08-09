8cc35fe7506c8b8e2b76c4bd0cddfdde3fcb4613
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-08-09 17:09:02 +0500

    coding

--- script.js
+++ script.js
@@ -8,17 +8,23 @@
 // var            //* Function scoped hota hai
 // var            //* You can redelcare and reintialize
 // var            //* window object ke sath attach hota hai
+// var            //* hoisting hota hai
 
 //! Second "let"
 // let c;         //* Delcare karna
 // let d = 10;    //* Delcare and intialize
 // let            //* block scope hota hai
 // let            //* You can just reintialize
+// let            //* hoisting hota hai but you can not access it before initialization
 
 //! third "const"
 // const a;       //* Esa declare karna allowed nahi hai
 // const b = 10;  //* Delcare and intialize
 // const          //* block scope hota hai
 // const          //* You can not reintialize and redeclare
+// const          //* hoisting hota hai but you can not access it before initialization
 
-
+ 
+console.log(a);
+a =  10;
+ 
