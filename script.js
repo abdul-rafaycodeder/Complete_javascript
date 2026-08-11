83007e1709608b306f64db39c8db8f51a8d38174
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-08-11 23:51:28 +0500

    coding

--- script.js
+++ script.js
@@ -283,3 +283,24 @@
 
 // console.log(result);
 
+// function getGrade(score) {
+//     if (score >= 90 && score <= 100) {
+//         return "A+"
+//     } else if (score >= 80 && score <= 89) {
+//         return "A"
+//     }
+//     else if (score >= 70 && score <= 79) {
+//         return "B"
+//     }
+//     else if (score >= 60 && score <= 69) {
+//         return "C"
+//     }
+//     else if (score >= 33 && score <= 59) {
+//         return "D"
+//     } 
+//     else if (score >= 0 && score <= z) {
+//         return "Fail"
+//     }
+// }
+
+
